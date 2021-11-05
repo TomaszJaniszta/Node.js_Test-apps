4c14a585d5113b27c4cc927c81651ee14417b752
TomaszJaniszta <92643157+TomaszJaniszta@users.noreply.github.com>
2021-11-05 20:07:48 +0100

    Create app.js

---
+++ zjazd1/zadanie9/app.js
@@ -0,0 +1,14 @@
+// Obsługa parametrów wejściowych. Aplikacja złożona jest z 1 pliku: `app.js`.
+// Command line arguments. The arguments are stored in process.argv - is an array containing the command line arguments. 
+// Podczas uruchamiania aplikacji możemy przekazać jej dodatkowy parametr, który zostanie wyświetlony po komunikacie `hello`. 
+// Gdy podanych będzie więcej parametrów, wtedy ignorujemy wszystkie prócz pierwszego. 
+// Gdy nie będzie podanych żadnych parametrów aplikacja wyświetla `hello world`. 
+
+const { argv } = require('process')
+
+if (argv.length >=3) {
+console.log("Hello " + argv[2])
+} else {
+console.log("Hello World")
+}
+
