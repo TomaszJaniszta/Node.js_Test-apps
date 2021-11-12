966e447f8d5a969f7ecf29dd5514f5a4e92a0a8e
TomaszJaniszta <92643157+TomaszJaniszta@users.noreply.github.com>
2021-11-12 10:02:51 +0100

    Update app.js

--- zjazd1/zadanie9/app.js
+++ zjazd1/zadanie9/app.js
@@ -5,10 +5,20 @@
 // Gdy nie będzie podanych żadnych parametrów aplikacja wyświetla `hello world`. 
 
 const { argv } = require('process')
+// let first_value = process.argv[2];
+
+//let param = (!first_value) ? 'world' : first_value; // inna forma ifa, poniżej rozwinięcie
+// if (!fisrt_value) {
+//     param = 'world';
+// }else{
+//     param = fisrt_value
+// }
+
 
 if (argv.length >=3) {
 console.log("Hello " + argv[2])
 } else {
 console.log("Hello World")
 }
+
 
