e91e2300455e2b171ab20e1adbcf2db46f62a5ee
TomaszJaniszta <92643157+TomaszJaniszta@users.noreply.github.com>
2021-11-12 10:19:13 +0100

    Create app.js

---
+++ zjazd2/zadanie1/app.js
@@ -0,0 +1,16 @@
+// zostanie zaimplementowana funkcja która zwraca nową tablicę bez zduplikowanych elementów.
+// Przykładowy kawałek kodu app.js:
+
+const someArray = ['ala', 3, 'ma', 'kota', 2, 'ala', 5, 3];
+const utils=require()
+
+const funcResult = utils.uniq(someArray);
+function add(array) {
+      someArray.forEach()
+    }
+
+
+
+console.log(funcResult);
+// Wynik:
+// [ 'ala', 3, 'ma', 'kota', 2, 5 ]
