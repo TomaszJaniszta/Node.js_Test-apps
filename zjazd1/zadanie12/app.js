8168fa6d397ee51e8aebedd08ebdad416d02b3c4
TomaszJaniszta <92643157+TomaszJaniszta@users.noreply.github.com>
2021-11-12 10:02:48 +0100

    Update app.js

--- zjazd1/zadanie12/app.js
+++ zjazd1/zadanie12/app.js
@@ -6,13 +6,13 @@ const fs = require('fs')
 //   console.log(argv[2])
 
 if (argv.length < 3) {
-console.log('zbyt mało parametrów!')
-} else if (argv.length > 3) {
-console.log('zbyt dużo parametrów!')
-} else {
-var data = fs.readdirSync(argv[2])
-console.log('Elementy w: ' + argv[2])
-data.forEach((item, index) => {
-console.log(`${index} : ${item}`)
+console.log('zbyt mało parametrów!')    
+    } else if (argv.length > 3) {
+    console.log('zbyt dużo parametrów!')
+        } else {
+            var data = fs.readdirSync(argv[2])
+            console.log('Elementy w: ' + argv[2])
+            data.forEach((item, index) => {
+            console.log(`${index} : ${item}`)
 });
 }
