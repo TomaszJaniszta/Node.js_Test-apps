dc64dd3e62663ce6d634d06348f9e5ba95a1c2aa
TomaszJaniszta <92643157+TomaszJaniszta@users.noreply.github.com>
2021-11-12 10:10:02 +0100

    Delete zjazd1 directory

--- zjazd1/zadanie7/app.js
+++
@@ -1,6 +0,0 @@
-// Aplikacja wyświetlająca informację o zalogowanym użytkowniku systemu, przy użyciu modułu OS. Dodatkowo: zapis nazwy użytkownika do pliku na dysku.
-const os = require('os')
-const username = os.userInfo().username //take user name from system
-console.log(username)
-const fs = require('fs')
-fs.writeFileSync('username.txt', username) //save user name in txt file on disc
