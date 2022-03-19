8a040f1c023cf565e406f92ed88f963439fabd0d
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-03-19 12:49:53 +0100

    Create zad2.js

---
+++ 20220306/zad2.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  res.writeHead(201, { "Content-type": "text/plain" }); //kod odpowiedzi, http status code
+    // res.writeHead(201, {"Content-type":"application/xml"}); //kod odpowiedzi 201, http status code
+  res.write("Hellow world");
+    // zamykamy połączenie 
+  res.end();
+});
+
+app.listen(4700);
