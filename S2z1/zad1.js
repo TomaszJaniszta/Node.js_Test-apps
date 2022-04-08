f63bc58622fdcd1ce1d00307dad87077479f7b2e
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-04-08 20:29:07 +0200

    Create zad1.js

---
+++ S2z1/zad1.js
@@ -0,0 +1,9 @@
+// https://api.openweathermap.org/data/2.5/weather?appid=0ed761300a2725ca778c07831ae64d6e&q=Bialystok
+const  http  =  require("http");
+// tworzymy nową instancję naszego serwera
+const  app  =  http.createServer((req,  res)  =>  {
+    // zamykamy połączenie wysyłając tekst
+    res.end("Hello  world");
+});
+// uruchamiamy nasz serwer na porcie 4700
+app.listen(4700);
