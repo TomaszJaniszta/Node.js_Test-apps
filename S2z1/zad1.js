7302dc5b070b3725bec6c021abe7a465d38948b1
Tomek <JANISZTAT@GMAIL.COM>
2022-07-29 13:07:22 +0200

    Delete zad1.js

--- S2z1/zad1.js
+++
@@ -1,8 +0,0 @@
-const  http  =  require("http");
-// tworzymy nową instancję naszego serwera
-const  app  =  http.createServer((req,  res)  =>  {
-    // zamykamy połączenie wysyłając tekst
-    res.end("Hello  world");
-});
-// uruchamiamy nasz serwer na porcie 4700
-app.listen(4700);
